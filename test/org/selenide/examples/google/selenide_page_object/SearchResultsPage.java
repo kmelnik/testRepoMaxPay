122ad68297dea44aa8b1f6dbfce514e5b1a93b9f
kmelnik <melnik_ks@ukr.net>
2018-03-23 14:45:02 +0200

    commit check open site (refresh code with page object)

--- test/org/selenide/examples/google/selenide_page_object/SearchResultsPage.java
+++ test/org/selenide/examples/google/selenide_page_object/SearchResultsPage.java
@@ -2,14 +2,17 @@ package org.selenide.examples.google.selenide_page_object;
 
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.FindBy;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class SearchResultsPage {
+  @FindBy (xpath = "//*[@id=\"applicationContainer\"]//div[2]/div/ul/li[1]/a/div/div")
+  private SelenideElement searchText;
   public SelenideElement getResults() {
-    return $$(By.xpath("//*[@id=\"applicationContainer\"]//div[2]/div/ul/li[1]/a/div/div")).findBy(text("КАЗИНО"));
+    return $(searchText);
   }
-
 }
+
--- test/org/selenide/examples/google/selenide_page_object/YourBetPages.java
+++ test/org/selenide/examples/google/selenide_page_object/YourBetPages.java
@@ -1,6 +1,8 @@
 package org.selenide.examples.google.selenide_page_object;
 
+import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.FindBy;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
@@ -13,4 +15,7 @@ public class YourBetPages {
   public SearchResultsPage searchForCasinoText (String text) {
     return page(SearchResultsPage.class);
   }
+
 }
+
+
