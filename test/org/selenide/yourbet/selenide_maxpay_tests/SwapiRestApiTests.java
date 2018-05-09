a0a41eb76d9664335189f4abb970530652fa58e1
kmelnik <melnik_ks@ukr.net>
2018-05-09 19:22:47 +0300

    MaxPay tests 9 commit 09-05-2018

--- test/org/selenide/yourbet/selenide_maxpay_tests/SwapiRestApiTests.java
+++ test/org/selenide/yourbet/selenide_maxpay_tests/SwapiRestApiTests.java
@@ -1,64 +1,65 @@
 package org.selenide.yourbet.selenide_maxpay_tests;
 
 
-import com.google.gson.Gson;
+import com.sun.xml.internal.ws.spi.db.BindingContextFactory;
 import org.json.simple.JSONArray;
-import org.junit.Assert;
+
 import org.junit.Test;
 
 import static com.jayway.restassured.RestAssured.get;
+import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 
 import com.jayway.restassured.response.Response;
 
 import org.selenide.yourbet.config_Properties_MaxPay.DataConfigMaxPay;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+
 import java.util.Iterator;
-import java.util.ListIterator;
+import java.util.logging.Logger;
+
 
 public class SwapiRestApiTests extends DataConfigMaxPay {
 
+    private Logger logger = Logger.getLogger(SwapiRestApiTests.class.getName());
 
     @Test
-    public void getRequestFindLukeSkywalkerData() {
+    public void getRequestFindSwapiApiData() {
 
         try {
-            // выполняем запрос get для доступа ко всем параметрам ответа
+            // выполняем запрос get для доступа ко всем параметрам ответа к базовому урлу АПИ
             Response response = get(getTestProperty("basic_swapi_url"));
             String responseBody = response.getBody().asString();
             System.out.println("Basic info API is " + responseBody);
 
+            // парсим джейсон для доступа к people
             org.json.simple.parser.JSONParser parserBasicInfo = new org.json.simple.parser.JSONParser();
             org.json.simple.JSONObject resultPeople = (org.json.simple.JSONObject) parserBasicInfo.parse(responseBody);
             String value = (String) resultPeople.get("people");
 
+            // получаем параметры ответа по Люку Скайволкеру
             Response responseResultPeople = get(value + getTestProperty("people_id"));
             String responseBodyPeople = responseResultPeople.getBody().asString();
+            logger.info("Searched people is  " + responseBodyPeople);
 
-            // получаем параметры ответа
-            System.out.println("Searched people is  " + responseBodyPeople);
-            // парсим джэйсон
+            // парсим джэйсон для доступа к name - Люку Скайволкеру
             org.json.simple.parser.JSONParser parser = new org.json.simple.parser.JSONParser();
             org.json.simple.JSONObject result = (org.json.simple.JSONObject) parser.parse(responseBodyPeople);
             String valueLuke = (String) result.get("name");
-            // получаем name человека
-
             assertEquals(valueLuke, "Luke Skywalker");
+            logger.info("Assert passed: \"Name\" belongs to \"Luke Skywalker\"");
 
+            // по homeworld ищем планету
             String homeworld = (String) result.get("homeworld");
-
             assertEquals(homeworld, "https://swapi.co/api/planets/1/");
 
             Response responseHomeworld = get(homeworld);
             String responseHomeworldBody = responseHomeworld.getBody().asString();
-            System.out.println("Lucke's Homeworld is  " + responseHomeworldBody);
+            logger.info("Lucke's Homeworld is  " + responseHomeworldBody);
 
-
+            // парсим джэйсон для проверки имени планеты и ее населения, проверяем assertEquals
             org.json.simple.parser.JSONParser parserHomeworldResponse = new org.json.simple.parser.JSONParser();
             org.json.simple.JSONObject resultHomeworldResponse = (org.json.simple.JSONObject) parserHomeworldResponse.parse(responseHomeworldBody);
 
@@ -66,62 +67,50 @@ public class SwapiRestApiTests extends DataConfigMaxPay {
             String valuesPopulationPlanet = (String) resultHomeworldResponse.get("population");
 
             assertEquals(valueNamePlanet, "Tatooine");
+            logger.info("Assert passed: \"Name\" belongs to \"Tatooine\"");
+
             assertEquals(valuesPopulationPlanet, "200000");
+            logger.info("Assert passed: \"population\" is \"200000\"");
 
-
+            // парсим джэйсон, используем паттерн итератор для поиска первого в списке этой планеты фильма
             JSONArray arrayFilms = (JSONArray) resultHomeworldResponse.get("films");
             Iterator<String> iterator1stFilmsOfArray = arrayFilms.iterator();
             Response responseFilmPage = get(iterator1stFilmsOfArray.next().toString());
             String responseFilmPageBody = responseFilmPage.getBody().asString();
-            System.out.println("First film in Lucke's Homeworld is  " + responseFilmPageBody);
+            logger.info("First film in Lucke's Homeworld is  " + responseFilmPageBody);
+
+            // находим title, проверяем совпадает ли с Attack of the Clones
             org.json.simple.parser.JSONParser parserFilmsResponse = new org.json.simple.parser.JSONParser();
             org.json.simple.JSONObject resultFilmResponse = (org.json.simple.JSONObject) parserFilmsResponse.parse(responseFilmPageBody);
             String valueFilmsTitle = (String) resultFilmResponse.get("title");
-
             assertEquals(valueFilmsTitle, "Attack of the Clones");
+            logger.info("Assert passed: \"title\" equals \"Attack of the Clones\"");
 
+            // проверяем принадлежит ли фильму - Lucke Skywalker и его планета
             try {
 
                 JSONArray arrayCharacters = (JSONArray) resultFilmResponse.get("characters");
                 assertTrue((arrayCharacters).contains(value + getTestProperty("people_id") + "/"));
-               // Iterator<String> iteratorArrayCharacters = arrayCharacters.iterator();
-               /* ListIterator<String> itArP = arrayCharacters.listIterator();
-
-                while (itArP.hasNext()) {
-
-                    Assert.assertEquals(value + getTestProperty("people_id") + "/", itArP.next());
-
-                }*/
+                logger.info("Assert passed: Lucke Skywalker is belong to this film \"Attack of the Clones\"");
 
             } catch (java.lang.AssertionError e) {
-                System.out.println(e);
+                logger.info("Assertion Error: Lucke Skywalker is not belong to film \"Attack of the Clones\"");
+            }
+
+            try {
+                JSONArray arrayPlanets = (JSONArray) resultFilmResponse.get("planets");
+                assertTrue((arrayPlanets).contains(homeworld));
+                logger.info("Assert passed: Lucke's Planet belong to film \"Attack of the Clones\"");
+
+
+            } catch (java.lang.AssertionError e) {
+                logger.info("Assertion Error: Lucke's Planet is not belong to film \"Attack of the Clones\"");
+
             }
 
 
-            JSONArray arrayPlanets = (JSONArray) resultFilmResponse.get("planets");
-            assertTrue((arrayPlanets).contains(homeworld));
-
-          /*  ListIterator<String> itArPl = arrayPlanets.listIterator();
-
-            while (itArPl.hasNext()) {
-
-              //  assertEquals(homeworld, arrayPlanets);
-
-                assertTrue((arrayPlanets).contains(homeworld));
-            }*/
-
-
-
-
-
-
-          /*  } catch (org.junit.ComparisonFailure e) {
-                System.out.println(e);
-            }*/
-
-
         } catch (org.json.simple.parser.ParseException e) {
-            System.out.println("Error: " + e);
+            logger.info("Parse Error: " + e);
         }
 
 
